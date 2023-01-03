a74b1d5c87f6b5118285f7d259115e57ed3674cd
Pulipuli Chen <pulipuli.chen@gmail.com>
2023-01-03 16:34:55 +0800

    Tue Jan  3 04:34:55 PM CST 2023

--- index.js
+++ index.js
@@ -2,8 +2,8 @@ const ShellSpawn = require('./app/lib/ShellSpawn')
 const GetExistedArgv = require('./app/lib/GetExistedArgv')
 const SetDockerComposeYML = require('./app/lib/SetDockerComposeYML')
 
-const fs = require('fs')
-const path = require('path')
+// const fs = require('fs')
+// const path = require('path')
 
 let main = async function () {
   // 1. 先取得輸入檔案的列表

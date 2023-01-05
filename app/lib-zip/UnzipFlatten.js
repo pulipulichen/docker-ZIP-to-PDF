cea09bc24fb81dbef3904f01a0f1b457414554b2
Pulipuli Chen <pulipuli.chen@gmail.com>
2023-01-05 16:29:22 +0800

    Thu Jan  5 04:29:22 PM CST 2023

--- app/lib-zip/UnzipFlatten.js
+++ app/lib-zip/UnzipFlatten.js
@@ -27,7 +27,7 @@ module.exports = async function(input, targetDir) {
   const entries = fg.sync([`${tmpDir}/**/*.*`], { dot: true, onlyFiles: true })
   for (let i = 0; i < entries.length; i++) {
     let entry = entries[i]
-		console.log('entry', entry)
+		// console.log('entry', entry)
     let name = path.basename(entry)
     // let target = `${targetDir}/${name}`
 		let target = `${targetDir}/${name}`
@@ -37,7 +37,7 @@ module.exports = async function(input, targetDir) {
 			// if (fs.statSync(target).isDirectory() === false) {
 			// 	target = path.dirname(target)
 			// }
-			console.log(['mv', entry, target])
+			// console.log(['mv', entry, target])
       await ShellSpawn(['mv', `"${entry}"`, `"${path.dirname(target)}"`])
     }
 		// else {
--- index.js
+++ index.js
@@ -21,27 +21,28 @@ let main = async function () {
 
 		// ------------
 
-		let filename = path.basename(file)
-		let dirname = path.dirname(file)
-		let tempFilename = Math.floor(Math.random() * 100000) + '.zip'
+		// let filename = path.basename(file)
+		// let dirname = path.dirname(file)
+		// let tempFilename = Math.floor(Math.random() * 100000) + '.zip'
 		
-		let tempFilePath = path.resolve(dirname, tempFilename)
-		// console.log(tempFilePath)
-		// console.log(path.resolve(dirname, filename.slice(0, -4) + '.txt'))
+		// let tempFilePath = path.resolve(dirname, tempFilename)
+		// // console.log(tempFilePath)
+		// // console.log(path.resolve(dirname, filename.slice(0, -4) + '.txt'))
 
-		fs.renameSync(file, tempFilePath)
+		// fs.renameSync(file, tempFilePath)
 
 		// ------------
     
-    SetDockerComposeYML(tempFilePath)
+    // SetDockerComposeYML(tempFilePath)
+		SetDockerComposeYML(file)
     await ShellSpawn('docker-compose up')
 
 		// --------------
 
-		fs.renameSync(tempFilePath, file)
-		if (fs.existsSync(path.resolve(dirname, tempFilename.slice(0, -4)) + '.pdf')) {
-			fs.renameSync(path.resolve(dirname, tempFilename.slice(0, -4)) + '.pdf', path.resolve(dirname, filename.slice(0, -4)) + '.pdf')
-		}
+		// fs.renameSync(tempFilePath, file)
+		// if (fs.existsSync(path.resolve(dirname, tempFilename.slice(0, -4)) + '.pdf')) {
+		// 	fs.renameSync(path.resolve(dirname, tempFilename.slice(0, -4)) + '.pdf', path.resolve(dirname, filename.slice(0, -4)) + '.pdf')
+		// }
   }
 
 }
